6aafa92d49ea1837b74c9d97f6e772df12034628
rainof <13416303+rainof@users.noreply.github.com>
2024-07-26 10:28:55 +0700

    Make qr code component for web-design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import qr_image from './assets/image-qr-code.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card" style={{ width: '20rem', padding: '1rem'}}>
+      <img className="card-img" src={qr_image} alt="qr code component" />
+      <div className="card-body">
+        <h5 className="card-title">Improve your front-end skills by building projects</h5>
+        <p className="card-text">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      </div>
     </div>
   );
 }
