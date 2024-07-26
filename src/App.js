80438537880b225de2fbef0e6d491752045cb520
rainof <13416303+rainof@users.noreply.github.com>
2024-07-26 12:09:32 +0700

    Make qr code component for mobile-design

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import qr_image from './assets/image-qr-code.png';
 
 function App() {
   return (
-    <div className="card" style={{ width: '20rem', padding: '1rem'}}>
-      <img className="card-img" src={qr_image} alt="qr code component" />
-      <div className="card-body">
-        <h5 className="card-title">Improve your front-end skills by building projects</h5>
-        <p className="card-text">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+    <div className='container'>
+      <div className="card" style={{ width: '20rem', padding: '1rem'}}>
+        <img className="card-img" src={qr_image} alt="qr code component" />
+        <div className="card-body">
+          <h5 className="card-title">Improve your front-end skills by building projects</h5>
+          <p className="card-text">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
       </div>
     </div>
   );
